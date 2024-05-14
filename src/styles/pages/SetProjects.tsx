5f7ee9259db7d53b41d18f1bcf156854d01667ba
kumble-amith <amithk2003@gmail.com>
2024-05-14 23:58:42 +0530

    first commit

--- src/styles/pages/SetProjects.tsx
+++ src/styles/pages/SetProjects.tsx
@@ -1,41 +1,43 @@
 import { Link } from "react-router-dom";
-import Logo from "../../../assets/laptop-image.jpeg"
-import Github from "../../../assets/github.jpeg"
+import Logo from "../../../assets/laptop-image.jpeg";
+import Github from "../../../assets/github.jpeg";
 
-interface Props{
-    project : any;
+interface Props {
+  project: any;
 }
-function SetProjects({project}: Props) {
-    const name = project.name;
-    const github = project.github;
-    const descriptions = project.description;
-    const date = project.date;
-    
-    return (
+function SetProjects({ project }: Props) {
+  const name = project.name;
+  const github = project.github;
+  const descriptions = project.description;
+  const date = project.date;
+
+  return (
     <>
-        <div className="each-project">
-            <div className="content">
-                <p className="project-title">{name}</p>
-                <div>
-                    {
-                        descriptions.map((description: string) => (
-                            <ul className="desc">
-                                <li>
-                                    <p className="bullet-head">&#x27A3;</p>{description}
-                                </li>
-                            </ul>
-                        ))
-                    }
-                </div>
-                <div className="pfooter"> 
-                    <Link to={github}><img src={Github} alt="Github"/></Link>
-                    <p>Date {date}</p>
-                </div>
-            </div>
-            <img src={Logo} alt="L" className="pimg"/>
+      Help Fuck Thilak
+      <div className="each-project">
+        <div className="content">
+          <p className="project-title">{name}</p>
+          <div>
+            {descriptions.map((description: string) => (
+              <ul className="desc">
+                <li>
+                  <p className="bullet-head">&#x27A3;</p>
+                  {description}
+                </li>
+              </ul>
+            ))}
+          </div>
+          <div className="pfooter">
+            <Link to={github}>
+              <img src={Github} alt="Github" />
+            </Link>
+            <p>Date {date}</p>
+          </div>
         </div>
+        <img src={Logo} alt="L" className="pimg" />
+      </div>
     </>
-  )
+  );
 }
 
-export default SetProjects
+export default SetProjects;
