78752ccbffb55a5ecf9a8a5184e63214b6232b57
kumble-amith <amithk2003@gmail.com>
2024-05-16 20:11:41 +0530

    contact added

--- src/styles/pages/Navbar.tsx
+++ src/styles/pages/Navbar.tsx
@@ -1,14 +1,31 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { Link } from "react-router-dom";
+import Insta from "../../../assets/insta.png";
+import Mail from "../../../assets/gmail.png";
+import Github from "../../../assets/github.jpeg";
 function Navbar() {
   return (
     <>
       <div className="navbar">
-        <AnchorLink href="#intro" className="link">
-          Home
-        </AnchorLink>
-        <AnchorLink href="#projects" className="link">
-          Projects
-        </AnchorLink>
+        <div className="contact">
+          <Link to="https://github.com/kumble-amith">
+            <img src={Github} alt="IG" />
+          </Link>
+          <Link to="mailto:amithk2003@gmail.com">
+            <img src={Mail} alt="IG" />
+          </Link>
+          <Link to="https://www.instagram.com/kumble_amith/">
+            <img src={Insta} alt="IG" />
+          </Link>
+        </div>
+        <div className="nav-links">
+          <AnchorLink href="#intro" className="link">
+            Home
+          </AnchorLink>
+          <AnchorLink href="#projects" className="link">
+            Projects
+          </AnchorLink>
+        </div>
       </div>
     </>
   );
--- src/styles/pages/Projects.tsx
+++ src/styles/pages/Projects.tsx
@@ -27,7 +27,7 @@ function Projects() {
       name: "Time Series Data Analysis",
       github: "",
       description: [
-        " This is a Integration of WEB and ML integrated using Django",
+        "This is a Integration of WEB and ML integrated using Django",
         "The csv is taken as an input to the algorithm.",
         "The data is read and then the model is built based on the user specified hyper-parameters.",
         "This act as a great visualizing tool for beginners as well.",
@@ -35,9 +35,26 @@ function Projects() {
       date: "5/24",
       photo: "time-data.png",
     },
-    // {
-
-    // }
+    {
+      name: "Optical Character Recognition",
+      github: "https://github.com/kumble-amith/OCR",
+      description: [
+        "It is a deep neural network based model trained on varios Hand Written Characters. If a character is given to the model then it properly predicts the English Alphabet.",
+        "It is a model trained using the CNN architecture and also has a pre-processing done. The accuracy of the model is around 81%",
+      ],
+      date: "10/23",
+      photo: "cnn.png",
+    },
+    {
+      name: "Expense Tracker",
+      github: "https://github.com/kumble-amith/expense-tracker",
+      description: [
+        "It is a platform where a person can handle their expenses.",
+        "The current implemented features are to only add and edit the expense of a person.Supabase is used as backed database to the project.",
+      ],
+      date: "4/24",
+      photo: "exp-track.png",
+    },
   ];
   return (
     <>
